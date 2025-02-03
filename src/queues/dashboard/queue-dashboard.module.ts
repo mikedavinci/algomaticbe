1e0804f5b25b588739e6d2d4b0ea0c30fc2aa6aa
Miguel Esparza Jr <mike@mikedavinci.com>
2025-02-03 01:06:37 -0500

    update

--- src/queues/dashboard/queue-dashboard.module.ts
+++ src/queues/dashboard/queue-dashboard.module.ts
@@ -1,24 +1,25 @@
-import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { join } from 'path';
 import { QueueModule } from '../queue.module';
 import { QueueDashboardController } from './queue-dashboard.controller';
 import { QueueDashboardGateway } from './queue-dashboard.gateway';
 import { AuthModule } from '../../auth/auth.module';
+import * as express from 'express';
 
 @Module({
   imports: [
     QueueModule,
     AuthModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, 'public'),
-      serveRoot: '/queue-dashboard',
-      serveStaticOptions: {
-        index: false,
-      },
-    }),
   ],
   controllers: [QueueDashboardController],
   providers: [QueueDashboardGateway],
 })
-export class QueueDashboardModule {}
+export class QueueDashboardModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(express.static(join(__dirname, 'public'), {
+        index: false,
+      }))
+      .forRoutes('/queue-dashboard/*');
+  }
+}
