796d1438a4a4f8eb5bfa5aa34357a26bc9d8ff7a
Miguel Esparza Jr <mike@mikedavinci.com>
2025-02-04 02:47:44 -0500

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { HasuraModule } from '../hasura/hasura.module';
 import { PassportModule } from '@nestjs/passport';
 import { ClerkClientProvider } from 'src/providers/clerk-client.provider';
+import { ClerkStrategy } from './clerk.strategy';
 
 @Module({
   imports: [ConfigModule, HasuraModule, PassportModule],
-  providers: [ClerkClientProvider],
+  providers: [ClerkClientProvider, ClerkStrategy],
   exports: [PassportModule],
 })
 export class AuthModule {}
